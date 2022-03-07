9d811169cab9895bd64bf053dccc42c311f76e30
Стасік Петрмк <stasps210@gmail.com>
2022-03-07 23:23:06 +0200

    update learn objects

--- js/objects.js
+++ js/objects.js
@@ -1,42 +1,32 @@
 let obj = {
   name: "stas", 
   age: 13,
+  bool: true,
   detals: {
     about: "lol"
   }
 }
 
-const x = [
-  {
-    "id":  1,
-    "masterID":  1,
-    "workingStartTime":  9,
-    "workingEndTime":  11,
-    "weekendDay":  true,
-    "date":  "2022-02-12",
-    "createdAt":  "2022-01-28T10:00:25.543Z",
-    "updatedAt":  "2022-01-28T10:00:25.543Z"
-  },
-  {
-    "id": 2,
-    "masterID": 1,
-    "workingStartTime": 12,
-    "workingEndTime": 15,
-    "weekendDay": null,
-    "date": "2022-02-07",
-    "createdAt": "2022-01-28T10:00:25.552Z",
-    "updatedAt": "2022-01-28T10:00:25.552Z"
-  }
-]
+console.log(obj.name)
+console.log(obj.age)
+console.log(obj.detals.about)
+console.log(obj.bool)
 
-let sum = 0
-
-for(let i = 0; i < x.length; i++) {
-  sum += x[i].workingStartTime
+let firstPart = "likes"
+let userInfo = {
+  name: "Stas",
+  let: 13,
+  [firstPart]: true,
 }
-console.log(sum)
+console.log(userInfo[firstPart])
 
+let key = "name"
+console.log(userInfo[key])
 
-// console.log(obj.name)
-// console.log(obj.age)
-// console.log(obj.detals.about)
+let id = Symbol("id")
+let userInfo2 = {
+  name: "name",
+  age: 1542,
+  [id]: ""
+}
+console.log(userInfo2)
