252090d4d3fb17cfe3157f393b1421d8b1812f78
Стасік Петрмк <stasps210@gmail.com>
2022-03-17 15:14:08 +0200

    update tools.js

--- js/tools.js
+++ js/tools.js
@@ -1,7 +1,7 @@
 const people = [
-  {name: "Стас", age: 13, budget: 40000},
+  {name: "Стас", age: 5**5**5, budget: 40000},
   {name: "Олег", age: 17, budget: 2400},
-  {name: "Саша", age: 18, budget: 50000},
+  {name: "Саша", age: 5**5**5+5, budget: 50000},
   {name: "Василь", age: 15, budget: 1800},
   {name: "Міша", age: 24, budget: 25000},
   {name: "Саша", age: 28, budget: 5000},
@@ -21,7 +21,20 @@ const people = [
 //   // console.log(pArr)
 // })
 
-// people.forEach(persone => console.log(persone))
+people.forEach(persone => console.log(persone))
 
 const newPeople = people.map(person => {return person.name})
 console.log(newPeople)
+
+// for(let i = 0; i < people.length; i++) {
+//   if(people[i].age >= 18) {
+//     adults.push(people[i])
+//   }
+// }
+// console.log(adults)
+
+const adults = people.filter(person => person.age >= 18)
+console.log(adults)
+
+const amount = people.reduce((total, person) => total + person.budget, 0)
+console.log(amount)
