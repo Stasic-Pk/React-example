6db7c60b093dcdfe0e8334ee581de57d270e9f8f
Стасік Петрмк <stasps210@gmail.com>
2022-03-29 22:37:46 +0300

    update asinc and await.js

--- js/asinc and await.js
+++ js/asinc and await.js
@@ -1,16 +1,29 @@
-async function x(time = 1000, x = 2) {
-  console.log(1)
+// async function x(time = 1000, x = 2) {
+//   setTimeout(() => {
+//     console.log(x)
+//   }, time);
+// }
+
+// x(6000, 1)
+// x()
+
+// async function y() {
+//   await x(4000, 4)
+// }
+
+// y()
+
+// console.log(3)
+
+let time = 0
+
+for(let i = 0; i < 15; i++) {
+  if(i % 2 == 0) {
+    time = i * 1000
+  } else{
+    time = i * 2000
+  }
   setTimeout(() => {
-    console.log(x)
+    console.log(i)
   }, time);
 }
-
-x()
-
-async function y() {
-  await x(4000)
-}
-
-y()
-
-console.log(3)
