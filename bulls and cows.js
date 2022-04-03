c979bc7b9100eda6903479160f3cd74b2ad41bc1
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-03 20:36:09 +0300

    feat: create input / exit functions

--- bulls and cows.js
+++ bulls and cows.js
@@ -1,28 +1,26 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
-})
-
-let number
-let randomNumbers = []
-let arrNum = []
-
-for(let i = 0; i < 4; i++) {
-  randomNumbers[i] = Math.floor(Math.random() * 9)
-}
-console.log(randomNumbers)
+});
 
 function input() {
-  for (let index = 0; index < 1; index++) {
-      readline.question("введите цифри ", number => {
-        readline.close()
-        console.log(index),
-        arrNum[index] = number
-        console.log(arrNum)
-      })
-  }
+  return new Promise((res, rej) => {
+    readline.question('Who are you?', name => {
+      res(name);
+    });
+  })
 }
 
-for (let i = 0; i < 40; i++) {
-  input()
+function exit() {
+  readline.close();
 }
+
+async function play() {
+  const x = await input();
+
+  const y = await input();
+
+  exit();
+}
+
+play()
