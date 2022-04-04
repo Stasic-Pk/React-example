bdfaf3a2126a7bdff276d35589a3a7fd28b4cb06
Стасік Петрмк <stasps210@gmail.com>
2022-04-04 23:10:27 +0300

    update bulls and cows.js

--- bulls and cows.js
+++ bulls and cows.js
@@ -1,12 +1,24 @@
+// const { Z_ASCII } = require('zlib');
+let randomNumber = []
+let number = ''
+let tags = []
+let q = 0
+
+
+for (let i = 0; i < 4; i++) {
+  randomNumber.push(Math.floor(Math.random() * 10));
+}
+console.log(randomNumber)
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function input() {
+function input(str) {
   return new Promise((res, rej) => {
-    readline.question('Who are you?', name => {
-      res(name);
+    readline.question(str, number => {
+      res(number);
     });
   })
 }
@@ -16,9 +28,41 @@ function exit() {
 }
 
 async function play() {
-  const x = await input();
+  while (true) {
+    inputNumber = await input(' > ');
 
-  const y = await input();
+    const number = Array.from(inputNumber);
+
+    for (let i = 0; i < 4; i++) {
+      if (number[i] == randomNumber[i]) {
+        tags[i] = "B"
+      } else {
+        for (let j = 0; j < 4; j++) {
+          if (number[i] == randomNumber[j]) {
+            tags[i] = "K"
+          }
+        }
+      }
+      q++
+      if (tags.length != q) {
+        tags[i] = "-"
+      }
+    }
+
+    console.log(...tags)
+
+    if (number == randomNumber) {
+      console.log("[+] you won")
+      break;
+    } else {
+      tags.length = 0;
+      q = 0
+    }
+
+    // if (va === 'x') {
+    //   break;
+    // }
+  }
 
   exit();
 }
