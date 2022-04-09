eae50c79b7bd842e5d83f4285d597b1e733c9564
Стасік Петрмк <stasps210@gmail.com>
2022-04-09 23:36:34 +0300

    update basics.js

--- OOP/basics.js
+++ OOP/basics.js
@@ -1,17 +1,19 @@
 class Person {
 
   name
+  age
 
-  constructor(name) {
+  constructor(name, age) {
     this.name = name
+    this.age = age
   }
 
   introduceSelf() {
-    console.log(`Hi! I'm ${this.name}`)
+    console.log(`Hi! I'm ${this.name}`, this.age)
   }
 
 }
 
-const consol = new Person('console');
+const consol = new Person('console', 12);
 
 consol.introduceSelf()
