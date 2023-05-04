046ccdd7297a38efc1b44006bb468cdf45952eae
Стасік Петрмк <stasps210@gmail.com>
2023-05-04 23:34:38 +0300

    restart learning react js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, {useState} from 'react'
-import Counter from './components/Counter'
-import PostItem from './components/postItem'
 import './styles/app.css'
-import PostList from './components/PostList'
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: "1.", title: "JS", body: "JS - prog language"},
-    {id: "2.", title: "JS", body: "JS - prog language"},
-    {id: "3.", title: "JS", body: "JS - prog language"},
-  ])
-
-  return(
-    <div className="app">
-      <PostList posts = {posts}/>
-    </div>
+  return (
+    <div className="app">    </div>
   )
 
 }
